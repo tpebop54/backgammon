d6699e1b929b1499029c6d366717cab570667b29
tarrasmith <tpebop54@gmail.com>
2025-06-26 19:28:56 -0600

    test check component

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,10 @@
 import BackgammonBoard from '@/components/BackgammonBoard';
+import Checker from '@/components/Checker';
 
 export default function Home() {
   return (
     <main>
-      <BackgammonBoard />
+      <Checker />
     </main>
   );
 }
